30d5838b36a154a73ae6fc55947e8817a2d19e69
Cody Vong <me@example.com>
2024-08-20 17:30:26 -0700

    Add project files.

---
+++ PaintingEstimate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+class PaintingEstimate
+{
+    static void Main()
+    {
+        int roomLength;
+        int roomWidth;
+        double paintCost;
+
+        Write("Enter length of the room in feet >> ");
+        roomLength = Convert.ToInt32(ReadLine());
+
+        Write("Enter width of room in feet >> ");
+        roomWidth = Convert.ToInt32(ReadLine());
+
+        paintCost = CostCalculation(roomLength, roomWidth);
+        WriteLine("Cost of painting the room is {0:C2}", paintCost);
+    }
+    private static double CostCalculation(int roomLength, int roomWidth)
+    {
+        int roomHeight = 15;
+        int jobPrice = 12;
+        double paintArea = (roomLength * roomHeight * 2) + (roomWidth * roomHeight * 2);
+        double paintRoomCost = jobPrice * paintArea;
+        return paintRoomCost;
+    }
+}
